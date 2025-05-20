4df818cdeb14f6851c45e98a56916716f0a7608c
Anthony Agby <anthonyagby@Anthonys-MacBook-Pro.local>
2025-05-20 14:52:26 -0400

    additional math for mean and PC

--- HyperSpectral/matrixMath.py
+++ HyperSpectral/matrixMath.py
@@ -1,9 +1,7 @@
 import numpy as np
 
 #calculating Eigen values, Eigen vectors, and the Coefficient Matrix for a matrix
-def matrixMath(valA1, valA2, valB1, valB2):
-    matrix = np.array([[valA1, valA2], [valB1, valB2]], dtype=float)
-
+def matrixMath(matrix):
     eigenValues, eigenVectors = np.linalg.eig(matrix)
 
     print('The eigenvalues of the covariance matrix are: ')
@@ -15,3 +13,26 @@ def matrixMath(valA1, valA2, valB1, valB2):
     coefficentMatrix = eigenVectors.transpose()
     print('The coefficient matrix is: ')
     print(coefficentMatrix)
+
+    return eigenValues, eigenVectors, coefficentMatrix
+
+#calculate mean of matrix
+def matrixMean(matrix):
+    mean = np.mean(matrix)
+    return mean
+
+#calculate principal components of matrix values and arrays of x,y coordinates
+def principalComponents(matrix, array1, array2):
+    mean = matrixMean(matrix)
+    eigenVal, eigenVectors, coefficientmatrix = matrixMath(matrix)
+
+    pcArray1 = np.matmul(coefficientmatrix, (array1-mean))
+    pcArray2 = np.matmul(coefficientmatrix, (array2-mean))    
+    
+    print('The Principal Component transformed value for array1 is: ');
+    print(pcArray1)  
+
+    print('The Principal Component transformed value for array2 is: ');
+    print(pcArray2)
+
+    return pcArray1, pcArray2
